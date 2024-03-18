aafd872c986d5a6dc51a0f5842f53526815d427c
sherzod <sherzodibodov098@gmail.com>
2024-03-18 13:02:32 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,106 @@
+// let month = prompt("oyni kiriting");
+// switch (month) {
+//   case "1":
+//     console.log("Yanvar");
+// }
+// switch (month) {
+//   case "2":
+//     console.log("February");
+// }
+// switch (month) {
+//   case "3":
+//     console.log("March ");
+// }
+// switch (month) {
+//   case "4":
+//     console.log("April");
+// }
+// switch (month) {
+//   case "5":
+//     console.log("May");
+// }
+// switch (month) {
+//   case "6":
+//     console.log("Jun");
+// }
+// switch (month) {
+//   case "7":
+//     console.log("July");
+// }
+// switch (month) {
+//   case "8":
+//     console.log("August");
+// }
+// switch (month) {
+//   case "9":
+//     console.log("Sentober");
+// }
+// switch (month) {
+//   case "10":
+//     console.log("Oktober");
+// }
+// switch (month) {
+//   case "11":
+//     console.log("November");
+// }
+// switch (month) {
+//   case "12":
+//     console.log("December");
+// }
+
+
+
+// let math = +prompt ('son kiriting');
+
+//  if( math % 3==0  && math % 5==0 ){ 
+// console.log('fithBath');
+// }
+//  else if(math % 3 == 0){
+//   console.log("fith");
+// }
+// else if(math % 5 == 0){
+//   console.log("bath");
+// }
+
+let month = +prompt("oyni kiriting");
+
+switch (month) {
+  case 1:
+    console.log("January");
+  break;
+  case 2:
+    alert("February");
+  break;
+  case 3:
+    alert("March");
+  break;
+  case 4:
+    alert("April");
+  break;
+  case 5:
+    alert("May");
+  break;
+  case 6:
+    alert("Jun");
+  break;
+  case 7:
+    alert("July");
+  break;
+  case 8:
+    alert("August");
+  break;
+  case 9:
+    alert("Sentober");
+  break;
+  case 10:
+    alert("Oktober");
+  break;
+  case 11:
+    alert("November");
+  break;
+  case 12:
+    alert("Desember");
+    break;
+  default:
+    alert("bunaqangi oy mavjud emas ");
+}
